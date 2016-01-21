34217012bb9dd257d368ea4af2f831bdb22dc6bc
liuyx <lyx2007825@126.com>
2016-01-21 14:12:52 +0800

    modify mcast-join

--- C2C/troop/mcast-join.c
+++ C2C/troop/mcast-join.c
@@ -1,18 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <net/if.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <net/if.h>
 #include <arpa/inet.h>
-#include <sys/ioctl.h>
-
-#define GROUP "225.0.0.37"
-#define PORT 12345
-#define BUF_SIZE 1024
-
-typedef struct sockaddr SA;
 
 static inline int family_to_level(int family) {
 	switch(family) {
@@ -53,7 +42,7 @@ int mcast_join(int sockfd, const SA *grp, socklen_t grplen, const char *ifname, 
 	return setsockopt(sockfd, family_to_level(grp->sa_family), MCAST_JOIN_GROUP, &req, sizeof(req));
 #else
 	switch (grp->sa_family) {
-		case AF_INET: {
+		case AF_INET:
 			struct ip_mreq mreq;
 			struct ifreq ifreq;
 			memcpy(&mreq.imr_multiaddr, &((const struct sockaddr_in *)grp)->sin_addr, sizeof(struct in_addr));
@@ -65,18 +54,17 @@ int mcast_join(int sockfd, const SA *grp, socklen_t grplen, const char *ifname, 
 				}
 				goto doioctl;
 			} else if (ifname != NULL) {
-				strncpy(ifreq.ifr_name, ifname, IFNAMSIZ);
+				strnctpy(ifreq.ifr_name, ifname, IFNAMSIZ);
 doioctl:
 				if (ioctl(sockfd, SIOCGIFADDR, &ifreq) < 0) {
 					return -1;
 				}
 				memcpy(&mreq.imr_interface, &((struct sockaddr_in *)&ifreq.ifr_addr)->sin_addr, sizeof(struct in_addr));
 			} else {
-				mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+				mreq.imr_interface = htonl(INADDR_ANY);
 			}
 
 			return setsockopt(sockfd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq));
-	}
 
 #ifdef IPV6
 		case AF_INET6:
@@ -86,53 +74,3 @@ doioctl:
 	return 0;
 #endif
 }
-
-int main(int argc, char **argv) {
-	int		sockfd, n;
-	const	int on = 1;
-	struct	sockaddr_in servaddr;
-	struct	ip_mreq mreq;
-	char	buff[BUF_SIZE];
-	socklen_t addrlen;
-
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror("socket");
-		exit(1);
-	}
-
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0) {
-		perror("setsockopt");
-		exit(1);
-	}
-
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(PORT);
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-	if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-		perror("bind");
-		exit(1);
-	}
-
-	struct sockaddr_in group;
-	group.sin_family = AF_INET;
-	if (inet_pton(AF_INET, GROUP, &group.sin_addr) < 0) {
-		perror("inet_pton");
-		exit(1);
-	}
-	mcast_join(sockfd, (struct sockaddr *)&group,sizeof(group),"eth0",0);
-	addrlen = sizeof(servaddr);
-
-	while (1) {
-		if ( (n = recvfrom(sockfd, buff, sizeof(buff), 0, (struct sockaddr *)&servaddr, &addrlen)) < 0) {
-			perror("recvfrom");
-			exit(1);
-		}
-		buff[n] = 0;
-		fputs(buff, stdout);
-	}
-
-	exit(0);
-}
-
